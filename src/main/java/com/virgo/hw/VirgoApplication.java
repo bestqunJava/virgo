eea943e1be20beaa3f378adcc087da7dbd258f66
wangchenkai <wangchenkai@wusongtech.com>
2020-06-28 16:01:47 +0800

    fire

---
+++ src/main/java/com/virgo/hw/VirgoApplication.java
@@ -0,0 +1,17 @@
+package com.virgo.hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * javadoc init start
+ * @author wang chenkai
+ */
+@SpringBootApplication
+public class VirgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirgoApplication.class, args);
+	}
+
+}
